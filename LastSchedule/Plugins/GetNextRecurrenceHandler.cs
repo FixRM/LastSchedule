774e3eddb84b43035f9f9bf2a792b5d5c8880e31
Artem Grunin <FixRM@users.noreply.github.com>
2019-06-10 16:43:21 +0500

    Initialize plugins project

---
+++ LastSchedule/Plugins/GetNextRecurrenceHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixRM.LastSchedule.Plugins
+{
+    public class GetNextRecurrenceHandler : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = serviceProvider.GetPluginExecutionContext();
+        }
+    }
+}
