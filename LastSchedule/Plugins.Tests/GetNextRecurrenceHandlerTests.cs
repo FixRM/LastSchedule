0513ddf8d35ce5c9a443ebd27c1f6ee0a5741894
Artem Grunin <FixRM@users.noreply.github.com>
2019-06-10 17:18:55 +0500

    Init test project

---
+++ LastSchedule/Plugins.Tests/GetNextRecurrenceHandlerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeXrmEasy;
+using FixRM.LastSchedule.Plugins;
+
+namespace Plugins.Tests
+{
+    [TestClass]
+    public class GetNextRecurrenceHandlerTests
+    {
+        [TestMethod]
+        public void DoNotThrowTest()
+        {
+            XrmFakedContext fakedContext = new XrmFakedContext();
+
+            XrmFakedPluginExecutionContext executionContext = fakedContext.GetDefaultPluginContext();
+
+            fakedContext.ExecutePluginWith<GetNextRecurrenceHandler>(executionContext);
+        }
+    }
+}
---
+++ LastSchedule/Plugins.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ce1bcabf-30d3-4599-bf1b-bafdfeb246dc")]
